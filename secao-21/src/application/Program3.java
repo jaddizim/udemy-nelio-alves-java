7e0be97362bed522311e2538b7da9c60aa981627
Jadder Freitas Souza <jaddizim@gmail.com>
2023-03-10 16:07:27 -0300

    "10/03/2023 : Melhoramento da conversão da data em String para java.sql.Date"

--- secao-21/src/application/Program3.java
+++ secao-21/src/application/Program3.java
@@ -3,18 +3,17 @@ package application;
 import db.DB;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Program3 {
 
     //Inserção de Dados
 
-    public static void main(String[] args) throws ParseException {
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    public static void main(String[] args) {
 
         PreparedStatement preparedStatement = null;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         try (Connection conn = DB.getConnection()) {
 
@@ -26,7 +25,7 @@ public class Program3 {
 
             preparedStatement.setString(1, "Carl Purple");
             preparedStatement.setString(2, "carl@gmail.com");
-            preparedStatement.setDate(3, new java.sql.Date(simpleDateFormat.parse("22/04/1985").getTime()));
+            preparedStatement.setDate(3, Date.valueOf(LocalDate.parse("22/04/1985", dtf)));
             preparedStatement.setDouble(4, 3000.0);
             preparedStatement.setInt(5, 4);
 
