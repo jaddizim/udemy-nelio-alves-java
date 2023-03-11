c0ca556170e3ce303b58f7cd484b9ae3bf236e10
Jadder Freitas Souza <jaddizim@gmail.com>
2023-03-10 22:40:32 -0300

    "10/03/2023 : Aplicando try-with-resources"

--- secao-21/src/application/Program2.java
+++ secao-21/src/application/Program2.java
@@ -13,26 +13,17 @@ public class Program2 {
 
     public static void main(String[] args) {
 
-        Connection conn;
-        Statement statement = null;
-        ResultSet resultSet = null;
-
-        try {
-            conn = DB.getConnection();
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM department");
+        try (Connection conn = DB.getConnection();
+             Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM department")) {
 
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1)
-                        + ", "
-                        + resultSet.getString(2));
+                        + ", " + resultSet.getString(2));
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            DB.closeResultset(resultSet);
-            DB.closeStatement(statement);
-            DB.closeConnection();
         }
     }
 }
