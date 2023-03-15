8f8be6ab3c090f0b913b96e2712fc919be7c5eac
Jadder Freitas Souza <jaddizim@gmail.com>
2023-03-15 09:12:51 -0300

    "15/03/2023 : Department entity class"

---
+++ secao-21/src/application/Program7.java
@@ -0,0 +1,9 @@
+package application;
+import model.entities.Department;
+
+public class Program7 {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
